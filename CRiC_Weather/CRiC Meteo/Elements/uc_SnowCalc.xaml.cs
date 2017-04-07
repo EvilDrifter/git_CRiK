9b950a719a84c84c8392597b0aa0c569092901f3
Evgenii <klenov.em@gmail.com>
2017-04-07 17:32:36 +0300

    Работает создание графика и привязка таблицы к функциям замерзания с стаивания

--- CRiC_Weather/CRiC Meteo/Elements/uc_SnowCalc.xaml.cs
+++ CRiC_Weather/CRiC Meteo/Elements/uc_SnowCalc.xaml.cs
@@ -1,51 +1,85 @@
 ﻿using CRiC_Meteo.Models;
 using CRiC_Meteo.Presenters;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CRiC_Meteo.Elements
 {
+    enum FunkFrozingMelting { накопление, таяние };
+
     public interface interface_OxyPlot
     {
         void UpdateFrozingMelting();
     }
 
-    enum FunkFrozingMelting { накопление, таяние };
+    public class TempValue
+    {
+        private double _t;
+        private double _v;
+
+        public double Temperature
+        {
+            get { return _t; }
+            set { _t = value; }
+        }
+        public double ValueByTemp
+        {
+            get { return _v; }
+            set { _v = value; }
+        }
+
+    }
+
+    public struct GraphInstal
+    {
+        public string TitleText, SubTitleText;
+        public int MaxX, MinX;
+        public int MaxY, MinY;
+        public string NameX, NameY;
+        public string NameSeries;
+    }
 
     public partial class uc_SnowCalc : UserControl
     {
         IPresenterOxyPlot prestnterOxyPlot;
 
-        public List<double> TempVal { get; set; }
-        public List<double> ValFromTemp { get; set; }
+        public ObservableCollection<TempValue> DataListForUpdate = new ObservableCollection<TempValue>();
+        private GraphInstal grIn;
 
         public uc_SnowCalc(IPresenterOxyPlot prestnterOxyPlot)
         {
             this.prestnterOxyPlot = prestnterOxyPlot;
             InitializeComponent();
+            grid_FrMelFunk.CellEditEnding += Grid_FrMelFunk_CellEditEnding;
+            cmb_basseinIndex.SelectionChanged += IndexComboboxChanged;
+            cmb_FrozingMelting.SelectionChanged += IndexComboboxChanged;
+            grid_FrMelFunk.DataContext = DataListForUpdate;
             UpdateComboBoxes();
+        }
+
+        private void IndexComboboxChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grIn = new GraphInstal();
+            UpdateTableWithFunk();
+            oxyPlot1.Model = OxyPlot_Funk.DrawFrMelFunk(DataListForUpdate, grIn);
+        }
+
+        private void Grid_FrMelFunk_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            int rowIndex = e.Row.GetIndex();
+            int colIndex = e.Column.DisplayIndex;
+            var el = e.EditingElement as TextBox;
+            double valueAfter = Convert.ToDouble(el.Text);
         }
 
         private void UpdateComboBoxes()
         {
             cmb_basseinIndex.ItemsSource = prestnterOxyPlot.lfm.Select(s => s.basseinIndex);
-            cmb_basseinIndex.SelectedIndex = 0;
             cmb_FrozingMelting.ItemsSource = Enum.GetValues(typeof(FunkFrozingMelting)).Cast<FunkFrozingMelting>();
-            cmb_FrozingMelting.SelectedIndex = 1;
-
-            UpdateTableWithFunk();
+            cmb_basseinIndex.SelectedItem = 0;
+            cmb_FrozingMelting.SelectedItem = 0;
         }
 
         private void UpdateTableWithFunk()
@@ -55,21 +89,32 @@ namespace CRiC_Meteo.Elements
             BasseinFrozingMelting t = new BasseinFrozingMelting();
             t = prestnterOxyPlot.lfm.Find(s => s.basseinIndex == index2);
 
+            DataListForUpdate.Clear();
+            grIn.NameX = "Температура [C]";
+            grIn.SubTitleText = index2.ToString();
+
             grid_FrMelFunk.Columns[0].Header = "Температура";
             if (index == 0)
             {
                 grid_FrMelFunk.Columns[1].Header = "% от осадков";
-                TempVal = t.frozintT;
-                ValFromTemp = t.frozingPer;
+                for (int i = 0; i < t.frozintT.Count; i++)
+                {
+                    DataListForUpdate.Add(new TempValue() { Temperature = t.frozintT[i], ValueByTemp = t.frozingPer[i] });
+                }
+                grIn.NameY = "% образовавшегося снега \n от выпавших остадков [%]";
+                grIn.NameSeries = "снегонакопление";
+
             }
             if (index == 1)
             {
                 grid_FrMelFunk.Columns[1].Header = "мм в час";
-                TempVal = t.meltingT;
-                ValFromTemp = t.meltingV;
+                for (int i = 0; i < t.meltingT.Count; i++)
+                {
+                    DataListForUpdate.Add(new TempValue() { Temperature = t.meltingT[i], ValueByTemp = t.meltingV[i] });
+                }
+                grIn.NameY = "скорость стаивания \n снега [мм/час]";
+                grIn.NameSeries = "стаивание";
             }
-
-
         }
     }
 }
